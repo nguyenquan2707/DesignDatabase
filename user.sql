78b9d600e7d1e3d71d0aad389296702e1333c512
Quan Nguyen <vozermuonnam1@gmail.com>
2023-11-12 11:08:21 +0700

    First commit

---
+++ user.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `user_addresses` DROP FOREIGN KEY `user_addresses_fk0`;
+
+ALTER TABLE `products` DROP FOREIGN KEY `products_fk0`;
+
+ALTER TABLE `products` DROP FOREIGN KEY `products_fk1`;
+
+ALTER TABLE `sub_categories` DROP FOREIGN KEY `sub_categories_fk0`;
+
